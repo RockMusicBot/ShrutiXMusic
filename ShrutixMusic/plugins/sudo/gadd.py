142c45bf6d691c562ff44d177baba53030ec3451
RockMusicBot <githubmusicbot@gmail.com>
2025-12-01 15:40:38 +0530

    Update gadd.py

--- ShrutixMusic/plugins/sudo/gadd.py
+++ ShrutixMusic/plugins/sudo/gadd.py
@@ -5,44 +5,41 @@ OWNERS = "8450725193"
 from ShrutixMusic import nand
 from ShrutixMusic.utils.database import add_served_chat, get_assistant
 
-
-@app.on_message(filters.command("gadd") & filters.user(int(OWNERS)))
+@nand.on_message(filters.command("gadd") & filters.user(int(OWNERS)))
 async def add_allbot(client, message):
     command_parts = message.text.split(" ")
     if len(command_parts) != 2:
-        await message.reply(
-            "**❍ ɪɴᴠᴀʟɪᴅ ᴄᴏᴍᴍᴀɴᴅ ғᴏʀᴍᴀᴛ. ᴘʟᴇᴀsᴇ ᴜsᴇ ʟɪᴋᴇ » `/gadd Bot_username`**"
-        )
-        return
+        return await message.reply("❍ Invalid format, Use: `/gadd bot_username`")
 
     bot_username = command_parts[1]
+
     try:
         userbot = await get_assistant(message.chat.id)
-        bot = await app.get_users(bot_username)
-        app_id = bot.id
+        bot = await nand.get_users(bot_username)
+        bot_id = bot.id
+
         done = 0
         failed = 0
-        lol = await message.reply("❍ **ᴀᴅᴅɪɴɢ ɢɪᴠᴇɴ ʙᴏᴛ ɪɴ ᴀʟʟ ᴄʜᴀᴛs!**")
-        await userbot.send_message(bot_username, f"/start")
+        status = await message.reply("❍ Adding bot in all chats...")
+
+        await userbot.send_message(bot_username, "/start")
+
         async for dialog in userbot.get_dialogs():
             if dialog.chat.id == -1002378673199:
                 continue
+
             try:
-
-                await userbot.add_chat_members(dialog.chat.id, app_id)
+                await userbot.add_chat_members(dialog.chat.id, bot_id)
                 done += 1
-                await lol.edit(
-                    f"**❍ ᴀᴅᴅɪɴɢ {bot_username}**\n\n**➥ ᴀᴅᴅᴇᴅ ɪɴ {done} ᴄʜᴀᴛs ✔**\n**➥ ғᴀɪʟᴇᴅ ɪɴ {failed} ᴄʜᴀᴛs ✘**\n\n**➲ ᴀᴅᴅᴇᴅ ʙʏ»** @{userbot.username}"
-                )
-            except Exception as e:
+            except:
                 failed += 1
-                await lol.edit(
-                    f"**❍ ᴀᴅᴅɪɴɢ {bot_username}**\n\n**➥ ᴀᴅᴅᴇᴅ ɪɴ {done} ᴄʜᴀᴛs ✔**\n**➥ ғᴀɪʟᴇᴅ ɪɴ {failed} ᴄʜᴀᴛs ✘**\n\n**➲ ᴀᴅᴅɪɴɢ ʙʏ»** @{userbot.username}"
-                )
-            await asyncio.sleep(3)  # Adjust sleep time based on rate limits
 
-        await lol.edit(
-            f"**❍ {bot_username} ʙᴏᴛ ᴀᴅᴅᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ🎉**\n\n**➥ ᴀᴅᴅᴇᴅ ɪɴ {done} ᴄʜᴀᴛs ✅**\n**➥ ғᴀɪʟᴇᴅ ɪɴ {failed} ᴄʜᴀᴛs ✘**\n\n**➲ ᴀᴅᴅᴇᴅ ʙʏ»** @{userbot.username}"
-        )
+            await status.edit(
+                f"Adding {bot_username}\n\n✔ Added: {done}\n✘ Failed: {failed}\nBy @{userbot.username}"
+            )
+            await asyncio.sleep(3)
+
+        await status.edit(f"🎉 Done! {bot_username} Added.\n\n✔ {done} chats\n✘ {failed} failed.")
+
     except Exception as e:
-        await message.reply(f"Error: {str(e)}")
+        await message.reply(f"Error: `{e}`")
